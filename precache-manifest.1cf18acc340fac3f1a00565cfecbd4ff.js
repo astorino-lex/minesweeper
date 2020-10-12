b78c548aede6564eb98d9e10045aef7aa6aa8c51
Alexa Astorino <alexa.astorino@gmail.com>
2020-10-12 11:02:12 -0600

    Updates

---
+++ precache-manifest.1cf18acc340fac3f1a00565cfecbd4ff.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c8276b82a7d421de86d52c1cb43f665",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "3aeb188d7d7c6f18fffc",
+    "url": "/minesweeper/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "15f3cca5b6a526454533",
+    "url": "/minesweeper/static/css/main.fa052362.chunk.css"
+  },
+  {
+    "revision": "3aeb188d7d7c6f18fffc",
+    "url": "/minesweeper/static/js/2.0f740928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minesweeper/static/js/2.0f740928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15f3cca5b6a526454533",
+    "url": "/minesweeper/static/js/main.cefbb1fb.chunk.js"
+  },
+  {
+    "revision": "03fdde6e651e9bdac82d",
+    "url": "/minesweeper/static/js/runtime-main.9c76a611.js"
+  },
+  {
+    "revision": "8e63a49e8cde4a758e7b0484523bc097",
+    "url": "/minesweeper/static/media/mine.8e63a49e.png"
+  },
+  {
+    "revision": "312bafc864a34d4dc061a1fd61b3f035",
+    "url": "/minesweeper/static/media/stopwatch.312bafc8.png"
+  }
+]);
